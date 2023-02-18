5dcea0690811d4e58e0cbc166af7bf40765ca21d
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-02-18 08:31:30 +0100

    fix input

--- src/input.rs
+++ src/input.rs
@@ -3,49 +3,52 @@ use std::io::Read;
 extern crate libc;
 
 pub struct Input {
-    termios: libc::Termios,
+    termios: libc::termios,
 }
 
 impl Input {
     pub fn new() -> Input {
-        let t = libc::Termios::new();
+        let mut t = libc::termios {
+            c_iflag: 0,
+            c_oflag: 0,
+            c_cflag: 0,
+            c_lflag: 0,
+            c_line: 0,
+            c_cc: [0; 32],
+            c_ispeed: 0,
+            c_ospeed: 0,
+        };
         unsafe {
-            libc::ioctl(0, libc::TCGETA, &t);
+            libc::tcgetattr(0, &mut t);
         }
 
         let input = Input { termios: t };
         input.cbreak();
-        input.nonblock();
         return input;
     }
 
     fn cbreak(&self) {
         let mut t = self.termios.clone();
         t.c_lflag &= !(libc::ICANON|libc::ECHO);
+        t.c_cc[libc::VMIN] = 0;
+        t.c_cc[libc::VTIME] = 0;
         unsafe {
-            libc::ioctl(0, libc::TCSETAW, &t);
-        }
-    }
-
-    fn nonblock(&self) {
-        unsafe {
-            let fl = libc::fcntl(0, libc::F_GETFL);
-            libc::fcntl(0, libc::F_SETFL, fl|libc::O_NONBLOCK);
+            libc::tcsetattr(0, libc::TCSADRAIN, &t);
         }
     }
 
     pub fn getch(&self) -> Option<u8> {
         let mut stdin = std::io::stdin();
         let mut buf = [0];
-        let count = stdin.read(&mut buf[..]).ok()?;
-        return Some(buf[0]);
+        stdin.read(&mut buf[..]).ok()?;
+        return if buf[0] == 0 { None } else { Some(buf[0]) };
     }
 }
 
 impl Drop for Input {
     fn drop(&mut self) {
         unsafe {
-            libc::ioctl(0, libc::TCSETAW, &self.termios);
+            libc::tcsetattr(0, libc::TCSADRAIN, &self.termios);
         }
     }
 }
--- src/libc.rs
+++ src/libc.rs
@@ -4,6 +4,7 @@ pub type c_int = i32;
 pub type c_uint = u32;
 pub type c_ulong = u64;
 pub type c_void = u8;
+pub type c_uchar = u8;
 pub type size_t = usize;
 pub type ssize_t = isize;
 pub type Ioctl = c_ulong;
@@ -11,40 +12,29 @@ pub type tcflag_t = c_uint;
 
 pub const SIGINT: c_int = 2;
 pub const TIOCGWINSZ: Ioctl = 0x5413;
-pub const TCGETA: Ioctl = 0x5405;
-pub const TCSETAW: Ioctl = 0x5407;
 pub const ICANON: tcflag_t = 0x00000002;
 pub const ECHO: tcflag_t = 0x00000008;
-pub const F_GETFL: c_int = 3;
-pub const F_SETFL: c_int = 4;
-pub const O_NONBLOCK: c_int = 2048;
+pub const TCSADRAIN: c_int = 1;
+pub const VTIME: usize = 5;
+pub const VMIN: usize = 6;
 
-#[derive(Clone)]
-pub struct Termios {
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct termios {
     pub c_iflag: tcflag_t,
     pub c_oflag: tcflag_t,
     pub c_cflag: tcflag_t,
     pub c_lflag: tcflag_t,
-    pub c_line: u8,
-    pub c_cc: [u8; 8],
-}
-
-impl Termios {
-    pub fn new() -> Termios {
-        return Termios {
-            c_iflag: 0,
-            c_oflag: 0,
-            c_cflag: 0,
-            c_lflag: 0,
-            c_line: 0,
-            c_cc: [0; 8],
-        };
-    }
+    pub c_line: c_uchar,
+    pub c_cc: [c_uchar; 32],
+    pub c_ispeed: c_uint,
+    pub c_ospeed: c_uint,
 }
 
 extern "C" {
     pub fn ioctl(fd: c_int, req: c_ulong, ...) -> c_int;
-    pub fn fcntl(fd: c_int, cmd: c_int, ...) -> c_int;
+    pub fn tcgetattr(fd: c_int, termios: *mut termios) -> c_int;
+    pub fn tcsetattr(fd: c_int, optional_actions: c_int, termios: *const termios) -> c_int;
     pub fn signal(signum: c_int, handler: size_t) -> size_t;
     pub fn getrandom(buf: *mut c_void, buflen: size_t, flags: c_uint) -> ssize_t;
 }
