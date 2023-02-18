6f819c6a0868c07636b8945a96b8aebab6debb95
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-02-18 11:44:19 +0100

    create sprites

---
+++ ppm2rust.py
@@ -0,0 +1,25 @@
+import sys
+
+WIDTH = 18
+HEIGHT = 24
+
+def _next(i):
+	return next(i).rstrip()
+
+with open(sys.argv[1]) as fh:
+	lines = iter(fh)
+	assert _next(lines) == 'P3'
+	assert _next(lines).startswith('#')
+	assert _next(lines) == f'{WIDTH} {HEIGHT}'
+	assert _next(lines) == '255'
+	print(f'pub const SPRITE: Sprite = [')
+	for y in range(HEIGHT):
+		print('    [')
+		for x in range(WIDTH):
+			r = int(_next(lines), 10)
+			g = int(_next(lines), 10)
+			b = int(_next(lines), 10)
+			print(f'        [{r}, {g}, {b}],')
+		print('    ], [')
+	print('];')
+	print()
